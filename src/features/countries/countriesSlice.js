04eef488f9092317b21858f3c1ce5039fffde23a
markusVJH <markus.heinila@pp.inet.fi>
2023-09-15 12:27:26 +0300

    base url for countriesapi, and initializeCountries function

--- src/features/countries/countriesSlice.js
+++ src/features/countries/countriesSlice.js
@@ -1,13 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit"; 
+import countriesAPI from "../../services/countries";
 
 export const countriesSlice = createSlice({
   name: 'countries',
   initialState: {
     countries: [],
+    isLoading: true,
   },
 
-  reducers: {},
+  reducers: {
+    isLoading(state, action) {
+      state.isLoading = action.payload;
+    }
+  },
 
 });
+
+export const initializeCountries = () => {
+  return async (dispatch) => {
+    const countries = await countriesAPI.getAll();
+    setTimeout(() => dispatch(isLoading(false)),1000)
+  }
+}
+
+export const { isLoading } = countriesSlice.actions;
 
 export default countriesSlice.reducer;
