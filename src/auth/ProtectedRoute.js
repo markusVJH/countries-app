ac036eeb9083e017cca4a8f6b80ede94efca9917
markusVJH <markus.heinila@pp.inet.fi>
2023-09-22 14:25:36 +0300

    Protected route component

---
+++ src/auth/ProtectedRoute.js
@@ -0,0 +1,8 @@
+import { Navigate, Outlet } from "react-router-dom"
+
+const ProtectedRoute = ({user, children}) => {
+  if(!user) {
+    return <Navigate to="/login" />
+  }
+  return <Outlet />
+}
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -15,24 +15,25 @@ const Layout = () => {
         <Navbar bg="light" variant="light" style={{ borderBottom: '1px solid lightgray' } }>
           <Container>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav"  className="justify-content-center">
-              <Nav>
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
                 <LinkContainer to="/">
                   <Nav.Link>Home</Nav.Link>
                 </LinkContainer>
-                <li style={{ borderRight: '1px solid lightgray' }}></li>
                 <LinkContainer to="/countries">
                   <Nav.Link>Countries</Nav.Link>
                 </LinkContainer>
+            </Nav>
+            </Navbar.Collapse>
+            <Nav className="ml-auto">
                 <LinkContainer to="/login">
                   <Nav.Link>Login</Nav.Link>
                 </LinkContainer>
                 <LinkContainer to="/register">
                   <Nav.Link>Register</Nav.Link>
                 </LinkContainer>
-              </Nav>
+            </Nav>
               <Button onClick={() => logout}>Logout</Button>
-            </Navbar.Collapse>
           </Container>
         </Navbar>
       </Row>
