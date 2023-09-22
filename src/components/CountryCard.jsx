24daa8b257586a3b2a30416648937fd0f1230126
markusVJH <markus.heinila@pp.inet.fi>
2023-09-22 20:57:52 +0300

    names and flags centered

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -8,23 +8,27 @@ function CountryCard({ country }) {
   const formattedPopulation = population.toLocaleString();
 
   return (
-      <Col className="mt-5">
+      <Col className="mt-5 country-card">
         <LinkContainer
           to={`/countries/${country.name.common}`}
           state={{ country: country }}
         >
           <Card className="h-100">
+            <Card.Body className="d-flex flex-column">
+            <div class="text-center">
+              <Card.Title >{country.name.common}</Card.Title>
+              <div className='scroll'>
+              <Card.Subtitle className="mb-1 text-muted p-2" style={{ borderBottom: '1px solid lightgray'}}>
+                {country.name.official}
+              </Card.Subtitle>
+              </div>
           <img
             src={flags && flags.svg}
             alt={`${country.name.common} Flag`}
-            className="position-absolute end-0 mt-1 me-1"
-            style={{ width: '4rem', height: 'auto', border: '1px solid lightgray'}}
+            
+            style={{ width: '5rem', border: '1px solid lightgray'}}
           />
-            <Card.Body className="d-flex flex-column">
-              <Card.Title>{country.name.common}</Card.Title>
-              <Card.Subtitle className="mb-5 text-muted">
-                {country.name.official}
-              </Card.Subtitle>
+            </div>
               <ListGroup
                 variant="flush"
                 className="flex-grow-1 justify-content-end"
@@ -33,7 +37,7 @@ function CountryCard({ country }) {
                   <i className="bi bi-people me-2"></i>
                   {formattedPopulation}
                 </ListGroup.Item>
-                <ListGroup.Item>
+                <ListGroup.Item className='scroll'>
                   <i className="bi bi-translate me-2"></i>
                   {languages && Object.values(languages).join(', ')}
                 </ListGroup.Item>
