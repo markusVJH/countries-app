ca9a01c58ce4c4536857582ab8fad549b0268058
markusVJH <markus.heinila@pp.inet.fi>
2023-09-22 10:35:42 +0300

    image, weather and capital displayed in single countries page

--- src/components/CountriesSingle.jsx
+++ src/components/CountriesSingle.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Spinner } from 'react-bootstrap';
+import { Col, Container, Row, Spinner, Image, Button } from 'react-bootstrap';
 import { useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
@@ -45,7 +45,26 @@ if (loading) {
 
   return (
     <Container>
-      <div>Single Country will be here</div>
+      <Row className="mt-5">
+        <Col>
+        <Image thumbnail src={`https://source.unsplash.com/1600x900/?${country.capital}`} />
+        </Col>
+        <Col>
+        <h2 className='display-4'>{country.name.common}</h2>
+        <h3>{country.capital}</h3>
+        {!error && weather && (
+          <div>
+          <p>
+            It is <strong>{parseInt(weather.main.temp)}</strong> degrees in {country.capital} and {weather.weather[0].description}
+          </p>
+          <img src={`http://openweatherapp.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={`${weather.weather[0].description}`}/>
+          </div>
+        )}
+        </Col>
+      </Row>
+      <Col>
+      <Button></Button>
+      </Col>
     </Container>
   );
 };
