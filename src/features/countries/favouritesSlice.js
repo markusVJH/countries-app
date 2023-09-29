72f21e2d3dc7b9c4d81a9628c0587151aa62d823
markusVJH <markus.heinila@pp.inet.fi>
2023-09-29 14:15:11 +0300

    addfavorite reducer

--- src/features/countries/favouritesSlice.js
+++ src/features/countries/favouritesSlice.js
@@ -5,9 +5,15 @@ const favourites = [];
 export const favouritesSlice = createSlice({
   name: "favourites",
   initialState: {
-    favourites: favourites,
+    favourites,
   },
-  reducers: {}
+  reducers: {
+    addFavourite: (state, action) => {
+      if(state.favourites.some(fav => fav === action.payload)) state.favourites = [...state.favourites]
+      state.favourites = [...state.favourites, action.payload]
+      localStorage.setItem("favourites", JSON.stringify(state.favourites));
+  }
+}
 });
 
 export default favouritesSlice.reducer;
