df97c3ef341738efd6a55972536559b85f78f80a
markusVJH <markus.heinila@pp.inet.fi>
2023-09-30 19:48:30 +0300

    pretty login and register forms

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -17,25 +17,48 @@ const Login = () => {
   },[user, loading])
 
   return(
-    <div className='full-height'>
-      <input
-      type= "email"
-      value={email}
-      onChange={(e) => setEmail(e.target.value)}
-      placeholder="Email"
-      />
-      <input
-      type= "password"
-      value={password}
-      onChange={(e) => setPassword(e.target.value)}
-      placeholder="Password"
-      />
-      <Button variant='dark' onClick={() => loginWithEmailAndPassword(email, password)}>Login</Button>
-      <div>
-        Don't have an account?
-        <Link to="/register">Register</Link>
+    <div className="container mt-5 full-height">
+    <div className="row justify-content-center">
+      <div className="col-md-4">
+        <div className="card">
+          <div className="card-body">
+            <div>
+              <form>
+                <div>
+                  <h3>Countries App - Login</h3>
+                  <div className="form-group mt-3">
+                  <input
+                  className="form-control mt-1"
+                  type= "email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Email"
+                  />
+                  </div>
+                  <div className="form-group mt-3">
+                  <input
+                  className="form-control mt-1"
+                  type= "password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Password"
+                  />
+                  </div>
+                  <div className="form-group mt-2">
+                    <p>Don't have an account?  <Link to="/register">Register</Link></p>
+                  </div>
+
+                  <div className="d-grid gap-2 mt-3">
+                  <Button variant='dark' onClick={() => loginWithEmailAndPassword(email, password)}>Login</Button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+  </div>
   )
 
 };
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -23,7 +23,58 @@ const Register = () => {
   },[user, loading])
 
   return (
-    <div className='full-height'>
+    <div className="container mt-5 full-height">
+    <div className="row justify-content-center">
+      <div className="col-md-4">
+        <div className="card">
+          <div className="card-body">
+            <div>
+              <form>
+                <div>
+                  <h3>Countries App - Register</h3>
+                  <div className="form-group mt-3">
+                  <input
+                  className="form-control mt-1"
+                  type= "text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Full name"
+                  />
+                  </div>
+                  <div className="form-group mt-3">
+                  <input
+                  className="form-control mt-1"
+                  type= "email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Email"
+                  />
+                  </div>
+                  <div className="form-group mt-3">
+                  <input
+                  className="form-control mt-1"
+                  type= "password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Password"
+                  />
+                  </div>
+                  <div className="form-group mt-2">
+                  <p>Already have an account? <Link to="/login">Login</Link></p>
+                  </div>
+
+                  <div className="d-grid gap-2 mt-3">
+                  <Button variant='dark'onClick={register}>Register</Button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+/*     <div className='full-height'>
       <input
       type= "text"
       value={name}
@@ -47,7 +98,7 @@ const Register = () => {
         Already have an account?
         <Link to="/login">Login</Link>
       </div>
-    </div>
+    </div> */
   )
 
 };
