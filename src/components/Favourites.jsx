57d20dd7d0014ab303bf38ba8a4208f45f9fc1aa
markusVJH <markus.heinila@pp.inet.fi>
2023-09-30 16:31:31 +0300

    empty favorites message and window confirm method for clear favorites

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { initializeCountries } from "../features/countries/countriesSlice";
 import { clearFavourites } from "../features/countries/favouritesSlice";
 import CountryCard from "./CountryCard";
+import '../App.css'
 
 
 const Favourites = () => {
@@ -12,15 +13,25 @@ const Favourites = () => {
     const loading = useSelector((state) => state.countries.loading)
     const [search, setSearch] = useState("")
     const favouritesList = useSelector((state) => state.favourites.favourites)  
+
+    const emptyFavoritesMessage = favouritesList.length === 0 ? (
+      <p>You have no favorites yet! Add some by clicking the  <i
+      className='bi bi-heart text-danger m-1 p-1'/> icon on a country :)</p>
+    ) : null;
+  
+
     if (favouritesList !== null) {
         countriesList = countriesList.filter(c => favouritesList.includes(c.name.common))
     }
     else {
         countriesList = []
     }
+
     useEffect(() => {
         dispatch(initializeCountries())
     }, [dispatch])
+
+
     if (loading) {
         return (
             <Col className="text-center m-5">
@@ -48,13 +59,15 @@ const Favourites = () => {
                 onChange={(e) => setSearch(e.target.value)}
               />
             </Form>
+            <Button variant="danger" onClick={() => {
+              const confirmed = window.confirm("Are you sure you want to clear favorites?");
+              if (confirmed) {
+                dispatch(clearFavourites());
+              }
+            }}>Clear Favourites</Button>
           </Col>
         </Row>
-        <Row xs={2} md={3} lg={4} className=" g-3">
-        <Button onClick={() => {
-          dispatch(clearFavourites())
-        }}>Clear Favourites</Button>
-      </Row>
+        <div className="empty-favorites">{emptyFavoritesMessage}</div>
         <Row xs={2} md={3} lg={3} className=" g-3">
           {countriesList
             .filter((c) => {
@@ -62,7 +75,7 @@ const Favourites = () => {
             })
             .map((country) => (
               <CountryCard key={country.name.common} country={country} />
-            ))}
+              ))}
         </Row>
       </Container>)
 }
